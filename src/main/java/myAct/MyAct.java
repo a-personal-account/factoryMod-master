f432868f8058dd790646d5203a4d141e16b9c64b
Raz <razash@gmail.com>
2019-12-12 17:02:23 +0100

    Modified for use with ActLikeIt.

--- src/main/java/myAct/MyAct.java
+++ src/main/java/myAct/MyAct.java
@@ -1,26 +1,18 @@
 package myAct;
 
+import actlikeit.dungeons.CustomDungeon;
 import basemod.BaseMod;
-import basemod.abstracts.CustomSavable;
 import basemod.helpers.RelicType;
 import basemod.interfaces.EditRelicsSubscriber;
 import basemod.interfaces.EditStringsSubscriber;
 import basemod.interfaces.PostInitializeSubscriber;
 import com.evacipated.cardcrawl.modthespire.lib.SpireInitializer;
 import com.megacrit.cardcrawl.core.Settings;
-import com.megacrit.cardcrawl.dungeons.Exordium;
 import com.megacrit.cardcrawl.localization.*;
-import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.monsters.MonsterGroup;
-import com.megacrit.cardcrawl.monsters.beyond.OrbWalker;
-import com.megacrit.cardcrawl.monsters.city.SphericGuardian;
-import com.megacrit.cardcrawl.monsters.exordium.Sentry;
 import myAct.dungeons.Factory;
 import myAct.events.*;
 import myAct.intents.CustomIntent;
 import myAct.intents.SummonMiniBotIntent;
-import myAct.monsters.*;
-import myAct.patches.GetDungeonPatches;
 import myAct.relics.ChargeChargeCharge;
 import myAct.relics.Remembrance;
 import org.apache.logging.log4j.LogManager;
@@ -30,11 +22,8 @@ import org.apache.logging.log4j.Logger;
 public class MyAct implements
         PostInitializeSubscriber,
         EditStringsSubscriber,
-        EditRelicsSubscriber,
-        CustomSavable<Boolean> {
+        EditRelicsSubscriber {
     public static final Logger logger = LogManager.getLogger(MyAct.class.getSimpleName());
-
-    public static boolean wentToTheFactory = false;
 
     public static void initialize() {
         BaseMod.subscribe(new MyAct());
@@ -80,135 +69,21 @@ public class MyAct implements
         BaseMod.addEvent(TroubledShopkeep.ID, TroubledShopkeep.class, Factory.ID);
         BaseMod.addEvent(WindingCorridors.ID, WindingCorridors.class, Factory.ID);
 
-        // Weak Pool
-        BaseMod.addMonster(FactoryEncounterIDList.SMALL_MINIBOT_GANG_ENCOUNTER_ID, "4 Mini-Bots", () -> new MonsterGroup(
-                new AbstractMonster[]{
-                        new MiniBotBeamer(-400.0F, 0.0F),
-                        new MiniBotBuilderBuilder(-200.0F, 0.0F),
-                        new MiniBotRepair(0.0F, 0.0F),
-                        new MiniBotDebuff(200.0F, 0.0F)
-                }));
-        BaseMod.addMonster(FactoryEncounterIDList.PERSONNEL_SMALL_ENCOUNTER_ID, "2 Experiment Personnel", () -> new MonsterGroup(
-                new AbstractMonster[]{
-                        new ExpPersonnel(-250.0F, 0.0F, false),
-                        new ExpPersonnel(150.0F, 0.0F, true)
-                }
-        ));
-        BaseMod.addMonster(FactoryEncounterIDList.BIG_BOT_ENCOUNTER_ID, () -> new BigBot(10.0F, 0.0F));
-
-        // Strong Pool
-        BaseMod.addMonster(FactoryEncounterIDList.LARGE_MINIBOT_HORDE_ENCOUNTER_ID, "6 Mini-Bots", () -> new MonsterGroup(
-                new AbstractMonster[]{
-                        new MiniBotVirus(-450.0F, 0.0F),
-                        new MiniBotRepair(-300.0F, 250.0F),
-                        new MiniBotBuilderBuilder(-150.0F, 0.0F),
-                        new MiniBotDebuff(0.0F, 250.0F),
-                        new MiniBotBeamer(150.0F, 0.0F),
-                }));
-        BaseMod.addMonster(FactoryEncounterIDList.PERSONNEL_AND_BOTS_ENCOUNTER_ID, "Personnel and Mini-Bots", () -> new MonsterGroup(
-                new AbstractMonster[]{
-                        new MiniBotDebuff(-500.0F, 0.0F),
-                        new MiniBotBeamer(-250.0F, 0.0F),
-                        new ExpPersonnel(0.0F, 0.0F, true),
-                        new ExpPersonnel(250.0F, 0.0F, false)
-                }));
-        BaseMod.addMonster(FactoryEncounterIDList.PERSONNEL_AND_ORB_ENCOUNTER_ID, "Personnel and Big Bot", () -> new MonsterGroup(
-                new AbstractMonster[]{
-                        new ExpPersonnel(-500.0F, 0.0F, true),
-                        new MiniBotVirus(-250.0F, 0.0F),
-                        new ExpPersonnel(0.0F, 0.0F, false),
-                        new MiniBotRepair(250.0F, 0.0F)
-                }
-        ));
-        BaseMod.addMonster(FactoryEncounterIDList.SHRAPNEL_THROWER_ENCOUNTER_ID, "Metal Thrower", () -> new MonsterGroup(
-                new AbstractMonster[]{
-                        new ShrapnelHeap(-300.0F, 0.0F),
-                        new ShrapnelTosser(50.F, 0.0F)
-                }
-        ));
-        BaseMod.addMonster(FactoryEncounterIDList.BOT_BUNDLE_ENCOUNTER_ID, "Big Bot Bundle", () -> new MonsterGroup(
-                new AbstractMonster[]{
-                        new MiniBotRepair(-500.0F, 0.0F),
-                        new SphericGuardian(-250.0F, 0.0F),
-                        new ToyOrb(-175.0F, 350.0F, 0),
-                        new Sentry(100.0F, 0.0F)
-                }
-        ));
-        BaseMod.addMonster(FactoryEncounterIDList.DEFECTIVE_SENTRY_ENCOUNTER_ID, () -> new DefectiveSentry(0.0F, 0.0F));
-        BaseMod.addMonster(FactoryEncounterIDList.BORING_BASEGAME_ENCOUNTER_ID, "Orbs and Orb Walker", () -> new MonsterGroup(
-                new AbstractMonster[]{
-                        new ToyOrb(-255.0F, 300.0F, 0),
-                        new OrbWalker(0.0F, 0.0F),
-                        new ToyOrb(200.0F, 250.0F, 1)
-                }
-        ));
-        BaseMod.addMonster(FactoryEncounterIDList.DOUBLE_BIG_BOT_ENCOUNTER_ID, "Sentries and Big Bot", () -> new MonsterGroup(
-                new AbstractMonster[]{
-                        new Sentry(-450.0F, 0.0F),
-                        new Sentry(-150.0F, 0.0F),
-                        new BigBot(150.0F, 0.0F)
-                }
-        ));
-        BaseMod.addMonster(FactoryEncounterIDList.DECAYING_SENTINEL_ENCOUNTER_ID, "Decaying Sentinel", () -> new MonsterGroup(
-                new AbstractMonster[]{
-                        new SentinelSpawn(-200.0F, 0.0F, 30),
-                        new DecayingSentinel(100.0F, 0.0F)
-                }
-        ));
-        BaseMod.addMonster(FactoryEncounterIDList.SENTRY_GUARDS_ENCOUNTER_ID, "Sentry and Guards", () -> new MonsterGroup(
-                new AbstractMonster[]{
-                        new MiniBotDebuff(-450.0F, 0.0F),
-                        new BigBot(-150.0F, 0.0F),
-                        new MiniBotRepair(150.0F, 0.0F)
-                }
-        ));
-
-        // Elite Pool
-        BaseMod.addMonster(FactoryEncounterIDList.MANSERVANTES_ENCOUNTER_ID, () -> new Manservantes(0.0F, 0.0F));
-        BaseMod.addMonster(FactoryEncounterIDList.SMOG_ELEMENTAL_ENCOUNTER_ID, () -> new SmogElemental(-50.0F, 75.0F));
-        BaseMod.addMonster(FactoryEncounterIDList.DRINK_BREWER_ENCOUNTER_ID, () -> new DrinkBrewer(0.0F, 0.0F));
-
-        // Bosses
-        BaseMod.addMonster(FactoryEncounterIDList.GUARDIAN_2_ENCOUNTER_ID, () -> new Guardian2(0.0F, 0.0F));
-        BaseMod.addMonster(FactoryEncounterIDList.EXPERIMENT_01_ENCOUNTER_ID, () -> new Experiment01(0.0F, 0.0F));
-        BaseMod.addMonster(FactoryEncounterIDList.SPIDER_ENCOUNTER_ID, () -> new SPIDER(0.0F, 100.0F));
-
-        // Add bosses here
-        BaseMod.addBoss(Factory.ID, Guardian2.ID, "superResources/images/map/placeholder.png", "superResources/images/map/placeholderOutline.png");
-        BaseMod.addBoss(Factory.ID, SPIDER.ID, "superResources/images/map/placeholder.png", "superResources/images/map/placeholderOutline.png"); // A R T
-        BaseMod.addBoss(Factory.ID, Experiment01.ID, "superResources/images/map/placeholder.png", "superResources/images/map/placeholderOutline.png");
-
         // Add intents
         MyAct.addIntent(new SummonMiniBotIntent());
 
         // Add dungeon
-        GetDungeonPatches.addDungeon(Factory.ID, Factory.builder());
-        GetDungeonPatches.addNextDungeon(Factory.ID, Exordium.ID);
-
-        //savable boolean
-        BaseMod.addSaveField("wentToTheFactory", this);
+        CustomDungeon.addAct(CustomDungeon.THEBEYOND, new Factory());
     }
 
     private void loadLocStrings(String language) {
-
-        if (language.equals("zhs")) {
-            BaseMod.loadCustomStringsFile(EventStrings.class, "superResources/localization/zhs/events.json");
-            BaseMod.loadCustomStringsFile(UIStrings.class, "superResources/localization/zhs/ui.json");
-            BaseMod.loadCustomStringsFile(MonsterStrings.class, "superResources/localization/zhs/monsters.json");
-            BaseMod.loadCustomStringsFile(PowerStrings.class, "superResources/localization/zhs/powers.json");
-            BaseMod.loadCustomStringsFile(RelicStrings.class, "superResources/localization/zhs/relics.json");
-            BaseMod.loadCustomStringsFile(ScoreBonusStrings.class, "superResources/localization/zhs/score_bonuses.json");
-            BaseMod.loadCustomStringsFile(BlightStrings.class, "superResources/localization/zhs/blights.json");
-        } else {
-            BaseMod.loadCustomStringsFile(EventStrings.class, "superResources/localization/eng/events.json");
-            BaseMod.loadCustomStringsFile(UIStrings.class, "superResources/localization/eng/ui.json");
-            BaseMod.loadCustomStringsFile(MonsterStrings.class, "superResources/localization/eng/monsters.json");
-            BaseMod.loadCustomStringsFile(PowerStrings.class, "superResources/localization/eng/powers.json");
-            BaseMod.loadCustomStringsFile(RelicStrings.class, "superResources/localization/eng/relics.json");
-            BaseMod.loadCustomStringsFile(ScoreBonusStrings.class, "superResources/localization/eng/score_bonuses.json");
-            BaseMod.loadCustomStringsFile(BlightStrings.class, "superResources/localization/eng/blights.json");
-        }
-
+        BaseMod.loadCustomStringsFile(EventStrings.class, "superResources/localization/" + language + "/events.json");
+        BaseMod.loadCustomStringsFile(UIStrings.class, "superResources/localization/" + language + "/ui.json");
+        BaseMod.loadCustomStringsFile(MonsterStrings.class, "superResources/localization/" + language + "/monsters.json");
+        BaseMod.loadCustomStringsFile(PowerStrings.class, "superResources/localization/" + language + "/powers.json");
+        BaseMod.loadCustomStringsFile(RelicStrings.class, "superResources/localization/" + language + "/relics.json");
+        BaseMod.loadCustomStringsFile(ScoreBonusStrings.class, "superResources/localization/" + language + "/score_bonuses.json");
+        BaseMod.loadCustomStringsFile(BlightStrings.class, "superResources/localization/" + language + "/blights.json");
     }
 
     @Override
@@ -217,23 +92,8 @@ public class MyAct implements
 
         // Load english first to avoid crashing if translation doesn't exist for something
         loadLocStrings("eng");
-        loadLocStrings(language);
-    }
-
-    @Override
-    public Boolean onSave() {
-        logger.info("Saving wentToTheFactory boolean: " + wentToTheFactory);
-        return wentToTheFactory;
-    }
-
-    @Override
-    public void onLoad(Boolean loadedBoolean) {
-        if (loadedBoolean != null) {
-            wentToTheFactory = loadedBoolean;
-        } else {
-            wentToTheFactory = false;
+        if(!language.equals("eng")) {
+            loadLocStrings(language);
         }
-        logger.info("Loading wentToTheFactory boolean: " + wentToTheFactory);
     }
-
 }
--- src/main/java/myAct/dungeons/Factory.java
+++ src/main/java/myAct/dungeons/Factory.java
@@ -1,80 +1,144 @@
 package myAct.dungeons;
 
-import com.badlogic.gdx.graphics.Color;
+import actlikeit.dungeons.CustomDungeon;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
-import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.helpers.ImageMaster;
 import com.megacrit.cardcrawl.localization.UIStrings;
-import com.megacrit.cardcrawl.map.MapRoomNode;
-import com.megacrit.cardcrawl.monsters.MonsterInfo;
-import com.megacrit.cardcrawl.rooms.EmptyRoom;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.monsters.MonsterGroup;
+import com.megacrit.cardcrawl.monsters.beyond.OrbWalker;
+import com.megacrit.cardcrawl.monsters.city.SphericGuardian;
+import com.megacrit.cardcrawl.monsters.exordium.Sentry;
 import com.megacrit.cardcrawl.saveAndContinue.SaveFile;
+import com.megacrit.cardcrawl.scenes.AbstractScene;
 import myAct.MyAct;
-import myAct.monsters.FactoryEncounterIDList;
-import myAct.patches.GetDungeonPatches;
+import myAct.monsters.*;
 import myAct.scenes.FactoryScene;
 
 import java.util.ArrayList;
 
-public class Factory extends AbstractDungeon {
+public class Factory extends CustomDungeon {
     public static final String ID = MyAct.makeID("Factory");
     private static final UIStrings uiStrings = CardCrawlGame.languagePack.getUIString(ID);
     public static final String[] TEXT = uiStrings.TEXT;
     public static final String NAME = TEXT[0];
     public static final String NUM = TEXT[1];
 
-    public Factory(AbstractPlayer p, ArrayList<String> emptyList) {
-        super(NAME, ID, p, emptyList);
+    public Factory() {
+        super(NAME, ID);
+        setMainMusic("superResources/audio/music/factory_main.ogg");
 
-        if (scene != null) {
-            scene.dispose();
-        }
-        scene = new FactoryScene(); // TODO
-        fadeColor = Color.valueOf("0f220aff");
+        addTempMusic("FACTORYELITE", "superResources/audio/music/factory_elite.ogg");
+        addTempMusic("BOSS_FACTORY", "superResources/audio/music/boss_factory.ogg");
 
-        initializeLevelSpecificChances();
-        mapRng = new com.megacrit.cardcrawl.random.Random(Settings.seed + AbstractDungeon.actNum * 100);
-        generateMap();
+        // Weak Pool
+        addMonster(FactoryEncounterIDList.SMALL_MINIBOT_GANG_ENCOUNTER_ID, "4 Mini-Bots", () -> new MonsterGroup(
+                new AbstractMonster[]{
+                        new MiniBotBeamer(-400.0F, 0.0F),
+                        new MiniBotBuilderBuilder(-200.0F, 0.0F),
+                        new MiniBotRepair(0.0F, 0.0F),
+                        new MiniBotDebuff(200.0F, 0.0F)
+                }));
+        addMonster(FactoryEncounterIDList.PERSONNEL_SMALL_ENCOUNTER_ID, "2 Experiment Personnel", () -> new MonsterGroup(
+                new AbstractMonster[]{
+                        new ExpPersonnel(-250.0F, 0.0F, false),
+                        new ExpPersonnel(150.0F, 0.0F, true)
+                }
+        ));
+        addMonster(FactoryEncounterIDList.BIG_BOT_ENCOUNTER_ID, () -> new BigBot(10.0F, 0.0F));
 
-        CardCrawlGame.music.changeBGM("FACTORYMAIN");
-        AbstractDungeon.currMapNode = new MapRoomNode(0, -1);
-        AbstractDungeon.currMapNode.room = new EmptyRoom();
+        // Strong Pool
+        addStrongMonster(FactoryEncounterIDList.LARGE_MINIBOT_HORDE_ENCOUNTER_ID, "6 Mini-Bots", () -> new MonsterGroup(
+                new AbstractMonster[]{
+                        new MiniBotVirus(-450.0F, 0.0F),
+                        new MiniBotRepair(-300.0F, 250.0F),
+                        new MiniBotBuilderBuilder(-150.0F, 0.0F),
+                        new MiniBotDebuff(0.0F, 250.0F),
+                        new MiniBotBeamer(150.0F, 0.0F),
+                }));
+        addStrongMonster(FactoryEncounterIDList.PERSONNEL_AND_BOTS_ENCOUNTER_ID, "Personnel and Mini-Bots", () -> new MonsterGroup(
+                new AbstractMonster[]{
+                        new MiniBotDebuff(-500.0F, 0.0F),
+                        new MiniBotBeamer(-250.0F, 0.0F),
+                        new ExpPersonnel(0.0F, 0.0F, true),
+                        new ExpPersonnel(250.0F, 0.0F, false)
+                }));
+        addStrongMonster(FactoryEncounterIDList.PERSONNEL_AND_ORB_ENCOUNTER_ID, "Personnel and Big Bot", () -> new MonsterGroup(
+                new AbstractMonster[]{
+                        new ExpPersonnel(-500.0F, 0.0F, true),
+                        new MiniBotVirus(-250.0F, 0.0F),
+                        new ExpPersonnel(0.0F, 0.0F, false),
+                        new MiniBotRepair(250.0F, 0.0F)
+                }
+        ));
+        addStrongMonster(FactoryEncounterIDList.SHRAPNEL_THROWER_ENCOUNTER_ID, "Metal Thrower", () -> new MonsterGroup(
+                new AbstractMonster[]{
+                        new ShrapnelHeap(-300.0F, 0.0F),
+                        new ShrapnelTosser(50.F, 0.0F)
+                }
+        ));
+        addStrongMonster(FactoryEncounterIDList.BOT_BUNDLE_ENCOUNTER_ID, "Big Bot Bundle", () -> new MonsterGroup(
+                new AbstractMonster[]{
+                        new MiniBotRepair(-500.0F, 0.0F),
+                        new SphericGuardian(-250.0F, 0.0F),
+                        new ToyOrb(-175.0F, 350.0F, 0),
+                        new Sentry(100.0F, 0.0F)
+                }
+        ));
+        addStrongMonster(FactoryEncounterIDList.DEFECTIVE_SENTRY_ENCOUNTER_ID, () -> new DefectiveSentry(0.0F, 0.0F));
+        addStrongMonster(FactoryEncounterIDList.BORING_BASEGAME_ENCOUNTER_ID, "Orbs and Orb Walker", () -> new MonsterGroup(
+                new AbstractMonster[]{
+                        new ToyOrb(-255.0F, 300.0F, 0),
+                        new OrbWalker(0.0F, 0.0F),
+                        new ToyOrb(200.0F, 250.0F, 1)
+                }
+        ));
+        addStrongMonster(FactoryEncounterIDList.DOUBLE_BIG_BOT_ENCOUNTER_ID, "Sentries and Big Bot", () -> new MonsterGroup(
+                new AbstractMonster[]{
+                        new Sentry(-450.0F, 0.0F),
+                        new Sentry(-150.0F, 0.0F),
+                        new BigBot(150.0F, 0.0F)
+                }
+        ));
+        addStrongMonster(FactoryEncounterIDList.DECAYING_SENTINEL_ENCOUNTER_ID, "Decaying Sentinel", () -> new MonsterGroup(
+                new AbstractMonster[]{
+                        new SentinelSpawn(-200.0F, 0.0F, 30),
+                        new DecayingSentinel(100.0F, 0.0F)
+                }
+        ));
+        addStrongMonster(FactoryEncounterIDList.SENTRY_GUARDS_ENCOUNTER_ID, "Sentry and Guards", () -> new MonsterGroup(
+                new AbstractMonster[]{
+                        new MiniBotDebuff(-450.0F, 0.0F),
+                        new BigBot(-150.0F, 0.0F),
+                        new MiniBotRepair(150.0F, 0.0F)
+                }
+        ));
+
+        // Elite Pool
+        addEliteEncounter(FactoryEncounterIDList.MANSERVANTES_ENCOUNTER_ID, () -> new Manservantes(0.0F, 0.0F));
+        addEliteEncounter(FactoryEncounterIDList.SMOG_ELEMENTAL_ENCOUNTER_ID, () -> new SmogElemental(-50.0F, 75.0F));
+        addEliteEncounter(FactoryEncounterIDList.DRINK_BREWER_ENCOUNTER_ID, () -> new DrinkBrewer(0.0F, 0.0F));
+
+        // Bosses
+        addBoss(FactoryEncounterIDList.GUARDIAN_2_ENCOUNTER_ID, () -> new Guardian2(0.0F, 0.0F), "superResources/images/map/placeholder.png", "superResources/images/map/placeholderOutline.png");
+        addBoss(FactoryEncounterIDList.EXPERIMENT_01_ENCOUNTER_ID, () -> new Experiment01(0.0F, 0.0F), "superResources/images/map/placeholder.png", "superResources/images/map/placeholderOutline.png"); // A R T
+        addBoss(FactoryEncounterIDList.SPIDER_ENCOUNTER_ID, () -> new SPIDER(0.0F, 100.0F), "superResources/images/map/placeholder.png", "superResources/images/map/placeholderOutline.png");
     }
 
-    public Factory(AbstractPlayer p, SaveFile saveFile) {
-        super(NAME, p, saveFile);
-
-        CardCrawlGame.dungeon = this;
-        if (scene != null) {
-            scene.dispose();
-        }
-        scene = new FactoryScene(); // TODO
-        fadeColor = Color.valueOf("0f220aff");
-
-        initializeLevelSpecificChances();
-        miscRng = new com.megacrit.cardcrawl.random.Random(Settings.seed + saveFile.floor_num);
-        CardCrawlGame.music.changeBGM("FACTORYMAIN");
-        mapRng = new com.megacrit.cardcrawl.random.Random(Settings.seed + saveFile.act_num * 100);
-        generateMap();
-        firstRoomChosen = true;
-
-        populatePathTaken(saveFile);
+    public Factory(CustomDungeon cd, AbstractPlayer p, ArrayList<String> emptyList)
+    {
+        super(cd, p, emptyList);
+    }
+    public Factory(CustomDungeon cd, AbstractPlayer p, SaveFile saveFile)
+    {
+        super(cd, p, saveFile);
     }
 
-    public static GetDungeonPatches.AbstractDungeonBuilder builder() {
-        return new GetDungeonPatches.AbstractDungeonBuilder() {
-            @Override
-            public AbstractDungeon build(AbstractPlayer p, ArrayList<String> theList) {
-                return new Factory(p, theList);
-            }
-
-            @Override
-            public AbstractDungeon build(AbstractPlayer p, SaveFile save) {
-                return new Factory(p, save);
-            }
-        };
+    @Override
+    public AbstractScene DungeonScene() {
+        return new FactoryScene();
     }
 
     @Override
@@ -96,55 +160,6 @@ public class Factory extends AbstractDungeon {
         } else {
             cardUpgradedChance = 0.5F;
         }
-
-}
-
-
-    @Override
-    protected void generateMonsters() {
-        // TODO: This is copied from TheCity
-        generateWeakEnemies(2);
-        generateStrongEnemies(12);
-        generateElites(10);
-    }
-
-    protected void generateWeakEnemies(int count) {
-        // TODO: This is copied from TheCity
-        ArrayList<MonsterInfo> monsters = new ArrayList<>();
-        monsters.add(new MonsterInfo(FactoryEncounterIDList.SMALL_MINIBOT_GANG_ENCOUNTER_ID, 2.0F));
-        monsters.add(new MonsterInfo(FactoryEncounterIDList.PERSONNEL_SMALL_ENCOUNTER_ID, 2.0F));
-        monsters.add(new MonsterInfo(FactoryEncounterIDList.BIG_BOT_ENCOUNTER_ID, 2.0F));
-        MonsterInfo.normalizeWeights(monsters);
-        populateMonsterList(monsters, count, false);
-    }
-
-    protected void generateStrongEnemies(int count) {
-        // TODO: This is copied from TheCity
-        ArrayList<MonsterInfo> monsters = new ArrayList<>();
-        monsters.add(new MonsterInfo(FactoryEncounterIDList.LARGE_MINIBOT_HORDE_ENCOUNTER_ID, 1.0F));
-        monsters.add(new MonsterInfo(FactoryEncounterIDList.PERSONNEL_AND_BOTS_ENCOUNTER_ID, 1.0F));
-        monsters.add(new MonsterInfo(FactoryEncounterIDList.SHRAPNEL_THROWER_ENCOUNTER_ID, 1.0F));
-        monsters.add(new MonsterInfo(FactoryEncounterIDList.PERSONNEL_AND_ORB_ENCOUNTER_ID, 1.0F));
-        monsters.add(new MonsterInfo(FactoryEncounterIDList.BOT_BUNDLE_ENCOUNTER_ID, 1.0F));
-        monsters.add(new MonsterInfo(FactoryEncounterIDList.DEFECTIVE_SENTRY_ENCOUNTER_ID, 1.0F));
-        monsters.add(new MonsterInfo(FactoryEncounterIDList.BORING_BASEGAME_ENCOUNTER_ID, 1.0F));
-        monsters.add(new MonsterInfo(FactoryEncounterIDList.DOUBLE_BIG_BOT_ENCOUNTER_ID, 1.0F));
-        monsters.add(new MonsterInfo(FactoryEncounterIDList.DECAYING_SENTINEL_ENCOUNTER_ID, 1.0F));
-        monsters.add(new MonsterInfo(FactoryEncounterIDList.SENTRY_GUARDS_ENCOUNTER_ID, 1.0F));
-
-        MonsterInfo.normalizeWeights(monsters);
-        populateFirstStrongEnemy(monsters, generateExclusions());
-        populateMonsterList(monsters, count, false);
-    }
-
-    protected void generateElites(int count) {
-        // TODO: This is copied from TheCity
-        ArrayList<MonsterInfo> monsters = new ArrayList<>();
-        monsters.add(new MonsterInfo(FactoryEncounterIDList.MANSERVANTES_ENCOUNTER_ID, 1.0F));
-        monsters.add(new MonsterInfo(FactoryEncounterIDList.DRINK_BREWER_ENCOUNTER_ID, 1.0F));
-        monsters.add(new MonsterInfo(FactoryEncounterIDList.SMOG_ELEMENTAL_ENCOUNTER_ID, 1.0f));
-        MonsterInfo.normalizeWeights(monsters);
-        populateMonsterList(monsters, count, true);
     }
 
     @Override
@@ -165,17 +180,6 @@ public class Factory extends AbstractDungeon {
         } */
         // TODO: THIS IS EXCLUSIVE ENCOUNTERS
         return retVal;
-    }
-
-    @Override
-    protected void initializeBoss() {
-        bossList.clear();
-        // Bosses are added via BaseMod in MyAct.receivePostInitialize()
-    }
-
-    @Override
-    protected void initializeEventList() {
-        // Events are added via BaseMod in MyAct.receivePostInitialize()
     }
 
     @Override
